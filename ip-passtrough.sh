94382d75e4b32fbd52e834dfb201fdabab41307e
slippern1 <93949744+slippern1@users.noreply.github.com>
2023-04-15 00:42:31 +0200

    Update ip-passtrough.sh

--- ip-passtrough.sh
+++ ip-passtrough.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+#This is a poor programmed script for creating an IP-Passtrough bridge for mobile connections. 
 
-#Bring up interfaces
+#Setting up mobile broadband connection
+sleep 10
+mmcli --scan-modems
+sleep 5
+mmcli -m 0 --set-allowed-modes='4g'
+sleep 5
+mmcli -m 0 --enable
+sleep 5
+mmcli -m 0 --simple-connect='apn=telenor.fwa,ip-type=ipv4'
+sleep 5
+
+#Setting up wwan0 and eht0 interfaces
 /usr/sbin/ifconfig wwan0 up
 /usr/sbin/ifconfig eth0 up
+
 #Get IPv4-address from modem
 IPV4_ADDRESS=$(mmcli --modem=0 --bearer=0 | grep address: | awk '{print $3}')
 echo "IPv4-Address: " $IPV4_ADDRESS
@@ -16,55 +29,74 @@ echo "IPv4-Subnet: " $IPV4_SUBNET_PREFIX
 IPV4_DNS_SERVER1=$(mmcli --modem=0 --bearer=0 | grep dns: | awk '{print $3}')
 IPV4_DNS_SERVER2=$(mmcli --modem=0 --bearer=0 | grep dns: | awk '{print $4}')
 
-#Remove "," from DNS-servers.
-IPV4_DNS_SERVER1=${IPV4_DNS_SERVER1%,*}
-IPV4_DNS_SERVER2=${IPV4_DNS_SERVER2%,*}
-echo "DNS-Server 1: " $IPV4_DNS_SERVER1
-echo "DNS-Server 2: " $IPV4_DNS_SERVER2
+IPCalc "30"
+if [[ $IPV4_DHCP_HOSTMIN == $IPV4_ADDRESS || $IPV4_DHCP_HOSTMAX == $IPV4_ADDRESS ]] && [ $IPV4_DHCP_BROADCAST != $IPV4_ADDRESS ]; then
+   echo "IP-en kan brukes i /30"
+   IPV4_Caclulator_Subnet="/30"
+   else
+      echo "/30 kan ikke brukes, sjekker /29"
+      IPCalc "29"
+      if [[ $IPV4_ADDRESS != $IPV4_DHCP_BROADCAST ]]; then
+          echo "$IPV4_ADDRESS kan brukes i /29"
+          IPV4_Caclulator_Subnet="/29"
+      else IPCalc "28"
+           if [[ $IPV4_ADDRESS != $IPV4_DHCP_BROADCAST ]]; then
+               echo "$IPV4_ADDRESS kan brukes i /28"
+               IPV4_Caclulator_Subnet="/28"
+           else IPCalc "27"
+                if [[ $IPV4_ADDRESS != $IPV4_DHCP_BROADCAST ]]; then
+                    echo "$IPV4_ADDRESS kan brukes i /27"
+                    IPV4_Caclulator_Subnet="/27"
+                else IPCalc "26"
+                     if [[ $IPV4_ADDRESS != $IPV4_DHCP_BROADCAST ]]; then
+                     echo "$IPV4_ADDRESS kan brukes i /26"
+                     IPV4_Caclulator_Subnet="/26"
+                     else IPCalc "25"
+                          if [[ $IPV4_ADDRESS != $IPV4_DHCP_BROADCAST ]]; then
+                          echo "$IPV4_ADDRESS kan brukes i /25"
+                          IPV4_Caclulator_Subnet="/25"
+                          else echo "Bruker /24-subnett"
+                               IPV4_Caclulator_Subnet="/24"
+                          fi
+                    fi
+                fi
+            fi
+       fi
+fi
+echo "Sjekker host min og host max i $IPV4_Caclulator_Subnet subnet"
+if [[ $IPV4_ADDRESS != $IPV4_DHCP_HOSTMIN ]]
+   then
+   echo "OK, IPV4-Adressen er ikke minst i subnettet, Gateway er god."
+   IPV4_GATEWAY=$IPV4_DHCP_HOSTMIN
+   echo "Gateway: $IPV4_GATEWAY, IP: $IPV4_ADDRESS"
+else
+   echo "Gateway blir host-max"
+   IPV4_GATEWAY=$IPV4_DHCP_HOSTMAX
+   echo "Gateway: $IPV4_GATEWAY, IP: $IPV4_ADDRESS"
+fi
 
+#Setting bridge interface and IP
+brctl addbr br0
+brctl addif br0 eth0
+brctl addif br0 wwan0
+ifconfig br0 up
 
-#Calculate IP-settings for DHCP-server
-#IPV4_DHCP_NETWORK=$(ipcalc -b $IPV4_ADDRESS/29 | grep Network: | awk '{print $2}')
-#IPV4_DHCP_NETMASK=$(ipcalc -b $IPV4_ADDRESS/29 | grep Netmask: | awk '{print $2}')
-#IPV4_DHCP_HOSTMIN=$(ipcalc -b $IPV4_ADDRESS/29 | grep HostMin: | awk '{print $2}')
-#IPV4_DHCP_HOSTMAX=$(ipcalc -b $IPV4_ADDRESS/29 | grep HostMax: | awk '{print $2}')
-#IPV4_DHCP_BROADCAST=$(ipcalc -b $IPV4_ADDRESS/29 | grep Broadcast: | awk '{print $2}')
+/usr/sbin/ip address add "$IPV4_GATEWAY/$IPV4_DHCP_NETMASK" broadcast "$IPV4_DHCP_BROADCAST" dev br0
 
-#Calculate IP-Settings for mirroring Telenor DHCP subnet
-IPV4_DHCP_NETWORK=$(ipcalc -b $IPV4_ADDRESS/$IPV4_SUBNET_PREFIX | grep Network: | awk '{print $2}')
-IPV4_DHCP_NETMASK=$(ipcalc -b $IPV4_ADDRESS/$IPV4_SUBNET_PREFIX | grep Netmask: | awk '{print $2}')
-IPV4_DHCP_HOSTMIN=$(ipcalc -b $IPV4_ADDRESS/$IPV4_SUBNET_PREFIX | grep HostMin: | awk '{print $2}')
-IPV4_DHCP_HOSTMAX=$(ipcalc -b $IPV4_ADDRESS/$IPV4_SUBNET_PREFIX | grep HostMax: | awk '{print $2}')
-IPV4_DHCP_BROADCAST=$(ipcalc -b $IPV4_ADDRESS/$IPV4_SUBNET_PREFIX | grep Broadcast: | awk '{print $2}')
-
-#Removing subnet-prefix from NETWORK.
+#Generate DHCP configuration:
 IPV4_DHCP_NETWORK_WITHOUT_SUBNET_PREFIX=${IPV4_DHCP_NETWORK%/*}
+
 echo "IPv4-DHCP-Network: " $IPV4_DHCP_NETWORK
 echo "IPv4-DHCP-Netmask: " $IPV4_DHCP_NETMASK
 echo "IPv4-DHCP-HostMin: " $IPV4_DHCP_HOSTMIN
 echo "IPv4-DHCP-HostMax: " $IPV4_DHCP_HOSTMAX
-
 echo "IPv4-Network-Without-Subnet-Prefix: " $IPV4_DHCP_NETWORK_WITHOUT_SUBNET_PREFIX
-
-#Creating bridge and adding interfaces to it.
-
-brctl addbr br0
-brctl addif br0 eth0
-brctl addif br0 wwan0
-
-#Bring up br0 interface
-/usr/sbin/ifconfig br0 up
-
-#Setting new static IP on eth0
-/usr/sbin/ip address add "$IPV4_DHCP_HOSTMAX/$IPV4_DHCP_NETMASK" broadcast "$IPV4_DHCP_BROADCAST" dev br0
-
-#Generate DHCP configuration:
 
 sed -i "s/subnet.*/subnet $IPV4_DHCP_NETWORK_WITHOUT_SUBNET_PREFIX netmask $IPV4_DHCP_NETMASK {/" /etc/dhcp/dhcpd.conf
 sed -i "s/range.*/range $IPV4_ADDRESS $IPV4_ADDRESS;/" /etc/dhcp/dhcpd.conf
 sed -i "s/option domain-name-servers.*/option domain-name-servers $IPV4_DNS_SERVER1, $IPV4_DNS_SERVER2;/" /etc/dhcp/dhcpd.conf
-sed -i "s/option broadcast-address.*/option broadcast-address $IPV4_DHCP_BROADCAST;/" /etc/dhcp/dhcpd.conf
-sed -i "s/option routers.*/option routers $IPV4_DHCP_HOSTMAX;/" /etc/dhcp/dhcpd.conf
+#sed -i "s/option broadcast-address.*/option broadcast-address $IPV4_DHCP_BROADCAST;/" /etc/dhcp/dhcpd.conf
+sed -i "s/option routers.*/option routers $IPV4_GATEWAY;/" /etc/dhcp/dhcpd.conf
 
 #Restaring DHCP-server
 /etc/init.d/isc-dhcp-server restart
